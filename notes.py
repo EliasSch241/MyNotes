10a05b62f4aff29a696c32853ec546759145c11e
EliasSch241 <162563495+EliasSch241@users.noreply.github.com>
2024-03-17 12:49:13 +0100

    Add files via upload

--- notes.py
+++ notes.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import benutzer, favoriten, notizen
+from app.models import Benutzer, Favoriten, Notizen
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'benutzer': benutzer, 'favoriten': favoriten, 'notizen': notizen}
+    return {'db': db, 'benutzer': Benutzer, 'favoriten': Favoriten, 'notizen': Notizen}
